35af46177aa8924c643daa8d65e0264a59feb61d
neu3n0 <neu3n0.sH@gmail.com>
2022-06-14 21:49:34 +0300

    init commit

---
+++ _vector.h
@@ -0,0 +1,19 @@
+namespace neu3n0 {
+
+
+
+
+template <class T>
+class vector {
+public:
+
+private:
+
+};
+
+
+
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "_vector.h"
+
+int main() {
+
+    return 0;
+}
